18fd2fa7f9c0a979b63409a7e69699633ae5fdd1
DeathByFork <yousifjabrallah@gmail.com>
2022-05-26 23:33:42 -0700

    added stars

--- game.js
+++ game.js
@@ -1,5 +1,6 @@
 import InputHandler from "./inputHandler.js";
 import Ship from "./ship.js";
+import Star from "./star.js";
 
 export default class Game {
     constructor(gameWidth, gameHeight){
@@ -9,14 +10,24 @@ export default class Game {
 
     start(){
         this.ship = new Ship(this.width, this.height);
+        this.stars = new Array(50);
+        for(let i = 0 ; i<this.stars.length; i++){
+            this.stars[i] = new Star(this.width, this.height);
+        }
         new InputHandler(this.ship);
     }
 
     update(deltaTime){
         this.ship.update(deltaTime);
+        for(let i = 0 ; i<this.stars.length; i++){
+            this.stars[i].update(deltaTime);
+        }
     }
 
     draw(ctx){
         this.ship.draw(ctx);
+        for(let i = 0 ; i<this.stars.length; i++){
+            this.stars[i].draw(ctx);
+        }
     }
 }
---
+++ star.js
@@ -0,0 +1,24 @@
+export default class Star{
+    constructor(gameWidth, gameHeight){
+        this.gameWidth = gameWidth;
+        this.gameHeight = gameHeight;
+        this.speed = 50;
+        this.position = {
+            x: Math.floor(Math.random()*gameWidth),
+            y: Math.floor(Math.random()*gameHeight)
+        };
+        this.size = 2;
+    }
+
+    draw(ctx){
+        ctx.fillStyle = 'white';
+        ctx.fillRect(this.position.x, this.position.y, this.size, this.size);
+    }
+
+    update(dt){
+        this.position.y += this.speed/dt;
+        if(this.position.y > this.gameHeight){
+            this.position.y = 0;
+        }
+    }
+}
